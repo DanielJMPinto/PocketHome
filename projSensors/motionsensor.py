7c2b6dbd40fd6bd45b45a2fdd04a53a43d19faa6
tomas99batista <tomas99batista@gmail.com>
2019-11-26 11:32:07 +0000

    mod motion sensor

--- projSensors/motionsensor.py
+++ projSensors/motionsensor.py
@@ -1,23 +1,23 @@
 import RPi.GPIO as GPIO
 import time
 
+# https://www.hackster.io/hardikrathod/pir-motion-sensor-with-raspberry-pi-415c04
+
 SENSOR_PIN = 23
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SENSOR_PIN, GPIO.IN)
-
-
-def mycallback(channel):
-	print("MOVEMENT!!!!")
-	# adicionar oq quisermos aqui
-	# pex. tirar uma foto c a camara
-
+i = 0
 try:
-	GPIO.add_event_detect(SENSOR_PIN, GPIO.RISING, callback=mycallback)
-	while True:
-		time.sleep(100)
-		print("\tWatching......")
+    time.sleep(2) # to stabilize sensor
+    while True:
+        if GPIO.input(23):
+            print("Motion Detected...")
+			print("Photo taken!")
+			i = i + 1
+    		camera.capture('./projSensors/img/image_%s.jpg' % i)
+    		print('A photo has been taken')
+			time.sleep(5) #to avoid multiple detection
+        time.sleep(0.1) #loop delay, should be less than detection delay
 
-except KeyboardInterrupt:
-	print("FINISHED!")
-
-GPIO.cleanup()
+except:
+    GPIO.cleanup()
