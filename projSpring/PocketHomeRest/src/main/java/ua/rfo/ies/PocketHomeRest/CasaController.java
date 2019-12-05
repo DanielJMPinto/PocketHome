ea7ae29b301be29eb1d441909d08462ba0e3c8d5
santorfo <39157156+santorfo@users.noreply.github.com>
2019-12-05 12:04:01 +0000

    CasaController

--- projSpring/PocketHomeRest/src/main/java/ua/rfo/ies/PocketHomeRest/CasaController.java
+++ projSpring/PocketHomeRest/src/main/java/ua/rfo/ies/PocketHomeRest/CasaController.java
@@ -1,9 +1,29 @@
 package ua.rfo.ies.PocketHomeRest;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CasaController {
+	@Autowired
+	private CasaRepository casarepository;
+	
+	@GetMapping("/houses")
+	public List<Casa> getAllHouses() {
+		return casarepository.findAll();
+	}
+	
+	@PostMapping("/houses")
+	public Casa createHouse(@Valid @RequestBody Casa house) {
+		return casarepository.save(house);
+	}
 	
 
 }
