8feddd94311d851a0c5281240698e53e35f878fb
tomas99batista <tomas99batista@gmail.com>
2019-12-10 01:00:21 +0000

    moved sender to sensors

---
+++ projSensors/sender.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import pika
+import datetime
+import uuid
+import json
+
+class Sender:
+    def __init__(self):
+        self.credentials = pika.PlainCredentials('tomas', 'tomas25')
+        self.connection = pika.BlockingConnection(
+                            pika.ConnectionParameters(host='192.168.43.40', 
+                                                        port=5672,
+                                                        virtual_host='/',
+                                                        credentials=self.credentials))
+
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='comm_channel')
+
+    def send(self, message):
+        message = {
+            'MESSAGE_ID': str(uuid.uuid4()),
+            'TIMESTAMP': str(datetime.datetime.now()),
+            'CONTENT': message
+        }
+        self.channel.basic_publish(exchange='', routing_key='comm_channel', body=json.dumps(message))
+        print(f" [x] Sent {message}!")
+
+sender = Sender()
+while True:
+    msg = input('Message: ')
+    if not msg:
+        sender.send('FINISHED_CONN')
+        print('bye')
+        sender.connection.close()
+        break
+    sender.send(msg)
+
+
