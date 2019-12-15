3d9989be2a1047af4fb5dcd49354a81ed2dd82ee
Daniel Pinto <dmatiaspinto@ua.pt>
2019-12-15 13:07:03 +0000

    css loading in html forms

--- projLogin/src/main/java/com/PocketHome/WebSecurityConfig.java
+++ projLogin/src/main/java/com/PocketHome/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/dist/**","/register").permitAll()
+                .antMatchers("/dist/**","/assets/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
