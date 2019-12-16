f2e5755012c26479527d30f5fbe869b2d74d900c
Flávia <flaviafigueiredo@ua.pt>
2019-12-16 20:38:15 +0000

    thymeleaf

--- projSpring/PocketHomeRest/src/main/java/ua/rfo/ies/PocketHomeRest/HomeService.java
+++ projSpring/PocketHomeRest/src/main/java/ua/rfo/ies/PocketHomeRest/HomeService.java
@@ -6,11 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
 public class HomeService {
-    @GetMapping({"/", "/index"})
-    public String hello(Model model) {
-        model.addAttribute("index", index);
-        return "index";
-    }
+
+
 }
--- projSpring/PocketHomeRest/src/main/java/ua/rfo/ies/PocketHomeRest/LogController.java
+++ projSpring/PocketHomeRest/src/main/java/ua/rfo/ies/PocketHomeRest/LogController.java
@@ -3,7 +3,6 @@ package ua.rfo.ies.PocketHomeRest;
 import java.util.List;
 
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
