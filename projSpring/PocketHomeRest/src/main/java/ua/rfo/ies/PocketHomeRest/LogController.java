77f7e4356cd74e8d67b8d2ba34a46f5eef176085
tomas99batista <tomas99batista@gmail.com>
2019-12-19 03:54:53 +0000

    kill me

--- projSpring/PocketHomeRest/src/main/java/ua/rfo/ies/PocketHomeRest/LogController.java
+++ projSpring/PocketHomeRest/src/main/java/ua/rfo/ies/PocketHomeRest/LogController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
+
 @RestController
 public class LogController {
 	@Autowired
